edb273f110e6780c2a772847f61499650f962c65
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2021-06-23 19:27:18 +0300

    feat: add Prettier and pre-commit hook

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,24 +1,24 @@
 /// <reference types="cypress" />
 
-context("Homepage", () => {
+context('Homepage', () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/");
+    cy.visit('http://localhost:3000/');
   });
 
   it('has correct title tag, meta tag "description", and favicon.ico', () => {
     cy.title().should(
-      "eq",
-      "Аренда автовышки и автокрана в Санкт-Петербурге и Ленинградской области"
+      'eq',
+      'Аренда автовышки и автокрана в Санкт-Петербурге и Ленинградской области'
     );
     cy.document()
       .get('head meta[name="description"]')
       .should(
-        "have.attr",
-        "content",
-        "Наша компания оказывает услуги по аренде автовышек и автокранов в Санкт-Петербурге и Ленинградской области"
+        'have.attr',
+        'content',
+        'Наша компания оказывает услуги по аренде автовышек и автокранов в Санкт-Петербурге и Ленинградской области'
       );
     cy.document()
       .get('head link[rel="icon"]')
-      .should("have.attr", "href", "/favicon.ico");
+      .should('have.attr', 'href', '/favicon.ico');
   });
 });
