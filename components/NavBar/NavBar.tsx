6340b7e13f378c5c6871dcb3d707ebe5adfc7482
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2021-07-12 14:08:02 +0300

    fix(navbar): click on triggering button is not considered as an outside click anymore

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -10,12 +10,14 @@ import styles from './NavBar.module.css';
 function NavBar() {
   const [menuOpen, setMenuOpen] = React.useState(false);
   const [subMenuOpen, setSubMenuOpen] = React.useState(false);
+  const menuWrapperEl = React.useRef<HTMLDivElement | null>(null);
   const menuEl = React.useRef<HTMLDivElement | null>(null);
+  const subMenuWrapperEl = React.useRef<HTMLDivElement | null>(null);
   const subMenuEl = React.useRef<HTMLDivElement | null>(null);
   const windowWidth = useWindowWidth();
 
-  useOnClickOutside(menuEl, () => setMenuOpen(false));
-  useOnClickOutside(subMenuEl, () => setSubMenuOpen(false));
+  useOnClickOutside(menuWrapperEl, () => setMenuOpen(false));
+  useOnClickOutside(subMenuWrapperEl, () => setSubMenuOpen(false));
 
   const menuHeight = menuOpen && menuEl.current?.scrollHeight ? menuEl.current.scrollHeight : 0;
   const subMenuHeight =
@@ -45,75 +47,77 @@ function NavBar() {
         </span>
       </h1>
 
-      <button
-        type="button"
-        className="md:hidden absolute -top-9 right-4 w-7 h-7 text-gray-800"
-        onClick={() => setMenuOpen(!menuOpen)}
-      >
-        <span className="sr-only">{menuOpen ? 'Закрыть меню' : 'Открыть меню'}</span>
-        <span className={menuOpen ? styles.menuBtnOpened : styles.menuBtn} />
-      </button>
+      <div ref={menuWrapperEl}>
+        <button
+          type="button"
+          className="md:hidden absolute -top-9 right-4 w-7 h-7 text-gray-800"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span className="sr-only">{menuOpen ? 'Закрыть меню' : 'Открыть меню'}</span>
+          <span className={menuOpen ? styles.menuBtnOpened : styles.menuBtn} />
+        </button>
 
-      <nav
-        className={menuOpen ? styles.menuOpened : styles.menu}
-        ref={menuEl}
-        style={windowWidth && windowWidth < 768 ? { height: menuHeight } : undefined}
-      >
-        <Link href="/">
-          <a className={styles.menuItem} onClick={() => setMenuOpen(false)}>
-            Главная
-          </a>
-        </Link>
-        <div className={styles.subMenuWrapper}>
-          {windowWidth && windowWidth < 768 ? (
-            <Link href="/#car-fleet">
-              <a className={styles.menuItem} onClick={() => setMenuOpen(false)}>
-                Автопарк
-              </a>
-            </Link>
-          ) : (
-            <button
-              type="button"
-              className={subMenuOpen ? styles.menuItemExpanded : styles.menuItemCollapsed}
-              onClick={() => setSubMenuOpen(!subMenuOpen)}
-            >
-              Автопарк
-            </button>
-          )}
-          <div
-            className={subMenuOpen ? styles.subMenuOpened : styles.subMenu}
-            ref={subMenuEl}
-            style={windowWidth && windowWidth >= 768 ? { height: subMenuHeight } : undefined}
-            data-testid="sub-menu"
-          >
-            {vehicles.map((vehicle) => (
-              <Link href={vehicle.path} key={vehicle.path}>
-                <a className={styles.subMenuItem}>{vehicle.name}</a>
+        <nav
+          className={menuOpen ? styles.menuOpened : styles.menu}
+          ref={menuEl}
+          style={windowWidth && windowWidth < 768 ? { height: menuHeight } : undefined}
+        >
+          <Link href="/">
+            <a className={styles.menuItem} onClick={() => setMenuOpen(false)}>
+              Главная
+            </a>
+          </Link>
+          <div className={styles.subMenuWrapper} ref={subMenuWrapperEl}>
+            {windowWidth && windowWidth < 768 ? (
+              <Link href="/#car-fleet">
+                <a className={styles.menuItem} onClick={() => setMenuOpen(false)}>
+                  Автопарк
+                </a>
               </Link>
-            ))}
+            ) : (
+              <button
+                type="button"
+                className={subMenuOpen ? styles.menuItemExpanded : styles.menuItemCollapsed}
+                onClick={() => setSubMenuOpen(!subMenuOpen)}
+              >
+                Автопарк
+              </button>
+            )}
+            <div
+              className={subMenuOpen ? styles.subMenuOpened : styles.subMenu}
+              ref={subMenuEl}
+              style={windowWidth && windowWidth >= 768 ? { height: subMenuHeight } : undefined}
+              data-testid="sub-menu"
+            >
+              {vehicles.map((vehicle) => (
+                <Link href={vehicle.path} key={vehicle.path}>
+                  <a className={styles.subMenuItem}>{vehicle.name}</a>
+                </Link>
+              ))}
+            </div>
           </div>
-        </div>
-        <Link href="/#service">
-          <a className={styles.menuItem} onClick={() => setMenuOpen(false)}>
-            Услуги
-          </a>
-        </Link>
-        <Link href="/#photo">
-          <a className={styles.menuItem} onClick={() => setMenuOpen(false)}>
-            Фото
-          </a>
-        </Link>
-        <Link href="/#reviews">
-          <a className={styles.menuItem} onClick={() => setMenuOpen(false)}>
-            Отзывы
-          </a>
-        </Link>
-        <Link href="/#contacts">
-          <a className={styles.menuItem} onClick={() => setMenuOpen(false)}>
-            Контакты
-          </a>
-        </Link>
-      </nav>
+          <Link href="/#service">
+            <a className={styles.menuItem} onClick={() => setMenuOpen(false)}>
+              Услуги
+            </a>
+          </Link>
+          <Link href="/#photo">
+            <a className={styles.menuItem} onClick={() => setMenuOpen(false)}>
+              Фото
+            </a>
+          </Link>
+          <Link href="/#reviews">
+            <a className={styles.menuItem} onClick={() => setMenuOpen(false)}>
+              Отзывы
+            </a>
+          </Link>
+          <Link href="/#contacts">
+            <a className={styles.menuItem} onClick={() => setMenuOpen(false)}>
+              Контакты
+            </a>
+          </Link>
+        </nav>
+      </div>
     </div>
   );
 }
