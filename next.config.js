448f46fbc87583c961f85d85b8eb029e72bc929d
Yury Nekhaevskiy <yury.nekhaevskiy@gmail.com>
2021-08-10 10:03:07 +0300

    feat: add redirect from /avtopark to /#vehicle-section

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/avtopark',
+        destination: '/#vehicle-section',
+        permanent: true,
+      },
+    ];
+  },
 };
